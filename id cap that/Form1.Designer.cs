1d8b5a9b8c46b302208990f01c77383b174c8d53
sabrina34352 <s.babakulova@student.inha.uz>
2021-12-11 20:37:42 +0500

    new app dropped

---
+++ id cap that/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace id_cap_that
+{
+    partial class shit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input = new System.Windows.Forms.PictureBox();
+            this.addpicture = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.addcaption = new System.Windows.Forms.Button();
+            this.getpicture = new System.Windows.Forms.OpenFileDialog();
+            this.caption = new System.Windows.Forms.TextBox();
+            this.domain = new System.Windows.Forms.DomainUpDown();
+            this.thecaption = new System.Windows.Forms.PictureBox();
+            this.save = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thecaption)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(12, 108);
+            this.input.MinimumSize = new System.Drawing.Size(460, 300);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(935, 477);
+            this.input.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.input.TabIndex = 0;
+            this.input.TabStop = false;
+            this.input.Click += new System.EventHandler(this.input_Click);
+            this.input.MouseMove += new System.Windows.Forms.MouseEventHandler(this.input_MouseMove);
+            // 
+            // addpicture
+            // 
+            this.addpicture.Location = new System.Drawing.Point(138, 69);
+            this.addpicture.Name = "addpicture";
+            this.addpicture.Size = new System.Drawing.Size(120, 33);
+            this.addpicture.TabIndex = 1;
+            this.addpicture.Text = "add a picture";
+            this.addpicture.UseVisualStyleBackColor = true;
+            this.addpicture.Click += new System.EventHandler(this.addpicture_Click);
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(264, 69);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(120, 33);
+            this.delete.TabIndex = 2;
+            this.delete.Text = "discard";
+            this.delete.UseVisualStyleBackColor = true;
+            // 
+            // addcaption
+            // 
+            this.addcaption.Location = new System.Drawing.Point(12, 69);
+            this.addcaption.Name = "addcaption";
+            this.addcaption.Size = new System.Drawing.Size(120, 33);
+            this.addcaption.TabIndex = 3;
+            this.addcaption.Text = "add caption";
+            this.addcaption.UseVisualStyleBackColor = true;
+            this.addcaption.Click += new System.EventHandler(this.addcaption_Click);
+            // 
+            // getpicture
+            // 
+            this.getpicture.FileName = "picture1";
+            this.getpicture.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // caption
+            // 
+            this.caption.Location = new System.Drawing.Point(12, 23);
+            this.caption.Multiline = true;
+            this.caption.Name = "caption";
+            this.caption.Size = new System.Drawing.Size(832, 40);
+            this.caption.TabIndex = 4;
+            this.caption.Text = "write text here";
+            this.caption.UseWaitCursor = true;
+            this.caption.TextChanged += new System.EventHandler(this.caption_TextChanged);
+            // 
+            // domain
+            // 
+            this.domain.Location = new System.Drawing.Point(390, 69);
+            this.domain.Name = "domain";
+            this.domain.Size = new System.Drawing.Size(51, 22);
+            this.domain.TabIndex = 5;
+            this.domain.Text = "1";
+            this.domain.SelectedItemChanged += new System.EventHandler(this.domain_SelectedItemChanged);
+            this.domain.Scroll += new System.Windows.Forms.ScrollEventHandler(this.domain_Scroll);
+            // 
+            // thecaption
+            // 
+            this.thecaption.BackColor = System.Drawing.SystemColors.Control;
+            this.thecaption.Location = new System.Drawing.Point(12, 120);
+            this.thecaption.Name = "thecaption";
+            this.thecaption.Size = new System.Drawing.Size(935, 465);
+            this.thecaption.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.thecaption.TabIndex = 6;
+            this.thecaption.TabStop = false;
+            this.thecaption.Click += new System.EventHandler(this.thecaption_Click);
+            this.thecaption.MouseDown += new System.Windows.Forms.MouseEventHandler(this.thecaption_MouseDown);
+            this.thecaption.MouseMove += new System.Windows.Forms.MouseEventHandler(this.thecaption_MouseMove);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(872, 74);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 7;
+            this.save.Text = "save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(447, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "color palette";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // shit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 597);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.thecaption);
+            this.Controls.Add(this.domain);
+            this.Controls.Add(this.caption);
+            this.Controls.Add(this.addcaption);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.addpicture);
+            this.Controls.Add(this.input);
+            this.MinimumSize = new System.Drawing.Size(500, 500);
+            this.Name = "shit";
+            this.Text = "capturing";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thecaption)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox input;
+        private System.Windows.Forms.Button addpicture;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button addcaption;
+        private System.Windows.Forms.OpenFileDialog getpicture;
+        private System.Windows.Forms.TextBox caption;
+        private System.Windows.Forms.DomainUpDown domain;
+        private System.Windows.Forms.PictureBox thecaption;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ id cap that/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace id_cap_that
+{
+    public partial class shit : Form
+    {
+        public Point current = new Point();
+        public Point old = new Point();
+        public Graphics g;
+        public shit()
+        {
+            InitializeComponent();
+            thenumbersindomain();
+            g=input.CreateGraphics();
+            input.Controls.Add(thecaption);
+            thecaption.BackColor = Color.Transparent;
+           
+        }
+
+        private void input_Click(object sender, EventArgs e)
+        {
+
+
+        }
+        public string chosen_file { get; set; }
+   
+        private void addpicture_Click(object sender, EventArgs e)
+        {
+
+            try 
+            {
+                getpicture.Title = "insert an image";
+                getpicture.Filter = "All|*.jpg;*.png| JPEG|*.jpg|PNG|*.png";
+                getpicture.ShowDialog();
+                chosen_file = getpicture.FileName;
+                Image img = Image.FromFile(chosen_file);
+                if (input.Width > img.Width && input.Height > img.Height)
+                {
+                    input.Width = img.Width;
+                    input.Height = img.Height;
+                }
+                else if (img.Width > input.Width && img.Height > input.Height)
+                {
+                    
+                    input.Width = img.Width / 2; input.Height = img.Height / 2;
+                    input.SizeMode = PictureBoxSizeMode.CenterImage;
+                }
+                input.Image = Image.FromFile(chosen_file);
+            }
+            catch { }
+        }
+
+        
+
+        private void addcaption_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textchanged();
+            }
+            catch { MessageBox.Show("Missing picture"); }
+        }
+
+        
+        public int fontsize{ get; set; }
+        public Font thefont { get; set; }
+        public Graphics gr { get; set; }
+        private void textchanged()
+        {
+            //Image image = (Image)Bitmap.FromFile(chosen_file);
+            var image = new Bitmap(this.thecaption.Width, this.thecaption.Height);
+            //Font font = new Font("DM Mono", fontsize);
+            thefont = new Font("DM Mono", fontsize);
+            gr = Graphics.FromImage(image);
+            gr.DrawString(caption.Text, thefont, Brushes.Green, 0,0);
+            this.thecaption.Image = image;
+            //font = thefont;
+            //image.Dispose();
+            
+        }
+
+        private void domain_SelectedItemChanged(object sender, EventArgs e)
+        {
+            fontsize = Convert.ToInt32(domain.Text);
+            
+        }
+
+        private void thenumbersindomain()
+        {
+            for(int i =99; i>=1; i--)
+            {
+                domain.Items.Add(i);
+                domain.Text = i.ToString();
+            }
+            domain.SelectedIndex = 79;
+
+        }
+
+        
+        private void thecaption_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            Point point = new Point(0,0);
+
+            if (e.Button == MouseButtons.Left)
+            {
+                thecaption.Left += e.X - point.X;
+                thecaption.Top += e.Y - point.Y;
+                
+            }
+
+        }
+        private void save_Click(object sender, EventArgs e)
+        {
+            //Image image = (Image)Bitmap.FromFile(chosen_file);
+
+
+            Graphics graphics;
+            Image inputimage = input.Image;
+            graphics = Graphics.FromImage(inputimage);
+            Font font = new Font("DM Mono", Convert.ToInt32(domain.Text));
+            graphics.DrawString(caption.Text, thefont, Brushes.Green, thecaption.Location);
+
+            
+            SaveFileDialog filesave = new SaveFileDialog();
+            filesave.Filter = "JPEG|*.jpg|PNG images|*.png";
+            if (filesave.ShowDialog() == DialogResult.OK) 
+                inputimage.Save(filesave.FileName);
+            inputimage.Dispose();
+
+        }
+
+
+
+
+
+
+
+
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e){}
+        private void domain_Scroll(object sender, ScrollEventArgs e){}
+        private void input_MouseMove(object sender, MouseEventArgs e){}
+        private void caption2_Click(object sender, EventArgs e){}
+        private void thecaption_Click(object sender, EventArgs e){}
+        private void thecaption_MouseDown(object sender, MouseEventArgs e){}
+        private void Form1_Load(object sender, EventArgs e){}
+        private void caption_TextChanged(object sender, EventArgs e){}
+
+        
+    }
+}
